55cef53c23a5391c446ff8dfbf46347b76da1142
chalermwut <webdevteam@touchtechnologies.co.th>
2021-03-31 14:53:37 +0700

    add: name

--- src/App.js
+++ src/App.js
@@ -8,26 +8,47 @@ function App() {
         <form>
           <div className='form-group'>
             <label htmlFor='email'>Email address</label>
-            <input type='text' id='email' placeholder='Enter name' />
+            <input
+              type='text'
+              id='email'
+              name='email'
+              placeholder='Enter name'
+            />
             <span>Test error message</span>
           </div>
           <div className='form-group'>
             <label htmlFor='first_name'>First name</label>
-            <input type='text' id='first_name' placeholder='Enter First name' />
+            <input
+              type='text'
+              id='first_name'
+              name='first_name'
+              placeholder='Enter First name'
+            />
           </div>
           <div className='form-group'>
             <label htmlFor='last_name'>Last name</label>
-            <input type='text' id='last_name' placeholder='Enter Last name' />
+            <input
+              type='text'
+              id='last_name'
+              name='last_name'
+              placeholder='Enter Last name'
+            />
           </div>
           <div className='form-group'>
             <label htmlFor='password'>Password</label>
-            <input type='password' id='password' placeholder='Password' />
+            <input
+              type='password'
+              id='password'
+              name='password'
+              placeholder='Password'
+            />
           </div>
           <div className='form-group'>
             <label htmlFor='confirmPassword'>Confirm Password</label>
             <input
               type='password'
               id='confirmPassword'
+              name='confirmPassword'
               placeholder='Enter Confirm Password'
             />
           </div>
